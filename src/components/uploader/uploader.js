f7290e6045fdcae5fceb4fa2e1009ee954d62997
Andre Luis Neto <andre.neto@globalmind.com.br>
2016-09-23 15:43:11 -0300

    Finished uploader.

---
+++ src/components/uploader/uploader.js
@@ -0,0 +1,10 @@
+/**
+ * @ngdoc module
+ * @name material.components.uploader
+ * @description Module for the uploader component.
+ */
+
+angular.module('material.components.uploader', [
+	'material.core',
+	'material.components.icon'
+]);
